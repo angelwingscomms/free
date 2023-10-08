b0bcd7cc4092c279a6b94a2cdbc8aa4081943755
EdwardGold Edem Hogan <edge37@outlook.com>
2023-10-08 00:56:39 +0000

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login/Sign Up</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            text-align: center;
-        }
+<script lang="ts">
+	import { Link } from 'carbon-components-svelte';
+</script>
 
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
+<svelte:head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Login/Sign Up</title>
+</svelte:head>
 
-        .button {
-            padding: 10px 20px;
-            font-size: 16px;
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-            margin: 10px;
-            border-radius: 5px;
-        }
-
-        .button:hover {
-            background-color: #0056b3;
-        }
-    </style>
-</head>
 <body>
-    <div class="container">
-        <h1>Welcome to our website</h1>
-        <p>Please log in or sign up</p>
-        <a href="/login" class="button">Log In</a>
-        <a href="/signup" class="button">Sign Up</a>
-    </div>
+	<div class="container">
+		<h1>Welcome to our website</h1>
+		<p>Please log in or sign up</p>
+		<Link href="/login" class="button">Log In</Link>
+		<Link href="/signup" class="button">Sign Up</Link>
+	</div>
 </body>
-</html>
+
+<style>
+	body {
+		font-family: Arial, sans-serif;
+		background-color: #f4f4f4;
+		text-align: center;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	.button {
+		padding: 10px 20px;
+		font-size: 16px;
+		background-color: #007bff;
+		color: #fff;
+		border: none;
+		cursor: pointer;
+		margin: 10px;
+		border-radius: 5px;
+	}
+
+	.button:hover {
+		background-color: #0056b3;
+	}
+</style>
