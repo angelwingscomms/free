b040c3bf664fcc711a4680c718bb3a835d9dcf39
Gold Edem Hogan <edge37@outlookcom>
2024-04-19 11:14:11 +0100

    .

--- src/routes/character/[id]/+page.server.ts
+++ src/routes/character/[id]/+page.server.ts
@@ -1,8 +1,9 @@
-import { client } from "$lib/util/redis"
-import { error } from "@sveltejs/kit"
+import { client } from '$lib/util/redis';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async({ request, params }) => {
-    const c = await client.json.get(params.id)
-    if (!c) throw error(404, `character {$params.id} not found`)
-    return { p: c.p, n: c.n}
-}
+export const load: PageServerLoad = async ({ params }) => {
+	const c = await client.json.get(params.id, { path: ['p', 'n'] });
+	if (!c) throw error(404, `character {$params.id} not found`);
+	return c;
+};
